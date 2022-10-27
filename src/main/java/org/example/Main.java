0136174f7a73f8de09a8374909f7a483c8700909
Tim Bashford <tim.bashford@uwtsd.ac.uk>
2022-10-27 12:41:40 +0100

    Added missing semicolon :(

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main
         System.out.println("Hello world!");
         System.out.printf("Hello world again\n");
         System.out.printf("This is another line of code.\n");
-        System.out.printf("This is a fourth line of code.\n")
+        System.out.printf("This is a fourth line of code.\n");
     }
 }
